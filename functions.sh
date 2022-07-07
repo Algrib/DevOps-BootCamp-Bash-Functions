233af9cbeeefa253d2891131e1441c814d59fb20
Aliaksandr Hrybovich <Aliaksandr_Hrybovich@epam.com>
2022-07-07 19:39:38 +0000

    replace print with print

--- functions.sh
+++ functions.sh
@@ -4,7 +4,7 @@ echo $(($1**$2))
 }
 shortest() {
 shortestArg=$(echo "${@}" | awk '{split($0,str," "); for (i in str) {print length(str[i])}}' | sort -n | head -n1)
-echo "${@}" | awk -v shortest="$shortestArg" '{split($0,str," "); for (i in str) {if (length(str[i]) == shortest) {print str[i]}}}'
+echo "${@}" | awk -v shortest="$shortestArg" '{split($0,str," "); for (i in str) {if (length(str[i]) == shortest) {printf str[i]"\n"}}}'
 }
 print_log() {
 echo $(date +[\%Y-\%m-\%d" "\%R]) $1
