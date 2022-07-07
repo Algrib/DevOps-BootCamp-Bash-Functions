69863f4f5b98ecd3161edb4c373e4a3044aac196
Aliaksandr Hrybovich <Aliaksandr_Hrybovich@epam.com>
2022-07-07 16:49:11 +0000

    add functions

--- functions.sh
+++ functions.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-
-# Put your code here
+pow() {
+echo $(($1**$2))
+}
+shortest() {
+shortestArg=$(echo "${@}" | awk '{split($0,str," "); for (i in str) {print length(str[i])}}' | sort -n | head -n1)
+echo "${@}" | awk -v shortest="$shortestArg" '{split($0,str," "); for (i in str) {if (length(str[i]) == shortest) {print str[i]}}}'
+}
+print_log() {
+echo $(date +[\%Y-\%m-\%d" "\%R]) $1
+}
